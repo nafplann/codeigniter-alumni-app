e523d3bd9274a19bb5466d839295075f3604fa86
Abdul Manaaf Saparuddin <nafplann@gmail.com>
2017-03-18 07:23:28 +0800

    update modul users

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -5,14 +5,175 @@ class Users extends MY_Controller {
   public function __construct()
   {
     parent::__construct();
+    
+    // Check apakah sudah login
     $this->cekLogin();
+
+    // Load model users
+    $this->load->model('model_users');
   }
 
   public function index()
   {
+    // Data untuk page index
     $data['pageTitle'] = 'Users';
-    $data['pageContent'] = '<h2>Daftar User</h2>';
+    $data['users'] = $this->model_users->get()->result();
+    $data['pageContent'] = $this->load->view('users/userList', $data, TRUE);
 
+    // Jalankan view template/layout
     $this->load->view('template/layout', $data);
+  }
+
+  public function add()
+  {
+    // Jika form di submit jalankan blok kode ini
+    if ($this->input->post('submit')) {
+      
+      // Mengatur validasi data username,
+      // # required = tidak boleh kosong
+      // # min_length[5] = username harus terdiri dari minimal 5 karakter
+      // # is_unique[users.username] = username harus bernilai unique, 
+      //   tidak boleh sama dengan record yg sudah ada pada tabel users
+      $this->form_validation->set_rules('username', 'Username', 'required|min_length[5]|is_unique[users.username]');
+
+      // Mengatur validasi data password,
+      // # required = tidak boleh kosong
+      // # min_length[5] = password harus terdiri dari minimal 5 karakter
+      $this->form_validation->set_rules('password', 'Password', 'required|min_length[5]');
+
+      // Mengatur validasi data level,
+      // # required = tidak boleh kosong
+      // # in_list[administrator, alumni] = hanya boleh bernilai 
+      //   salah satu di antara administrator atau alumni
+      $this->form_validation->set_rules('level', 'Level', 'required|in_list[administrator,alumni]');
+
+      // Mengatur validasi data active,
+      // # required = tidak boleh kosong
+      // # in_list[0, 1] = hanya boleh bernilai 
+      // salah satu di antara 0 atau 1
+      $this->form_validation->set_rules('active', 'Active', 'required|in_list[0,1]');
+
+      // Mengatur pesan error validasi data
+      $this->form_validation->set_message('required', '%s tidak boleh kosong!');
+      $this->form_validation->set_message('min_length', '%s minimal %d karakter!');
+
+      // Jalankan validasi jika semuanya benar maka lanjutkan
+			if ($this->form_validation->run() === TRUE) {
+
+        $data = array(
+          'username' => $this->input->post('username'),
+          'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
+          'level' => $this->input->post('level'),
+          'active' => $this->input->post('active')
+        );
+
+        // Jalankan function insert pada model_users
+        $query = $this->model_users->insert($data);
+
+        // cek jika query berhasil
+        if ($query) $message = array('status' => true, 'message' => 'Berhasil menambahkan user');
+        else $message = array('status' => true, 'message' => 'Gagal menambahkan user');
+
+        // simpan message sebagai session
+        $this->session->set_flashdata('message', $message);
+
+        // refresh page
+        redirect('users/add', 'refresh');
+			} 
+    }
+    
+    // Data untuk page users/add
+    $data['pageTitle'] = 'Tambah Data User';
+    $data['pageContent'] = $this->load->view('users/userAdd', $data, TRUE);
+
+    // Jalankan view template/layout
+    $this->load->view('template/layout', $data);
+  }
+
+  public function edit($id = null)
+  {
+    // Jika form di submit jalankan blok kode ini
+    if ($this->input->post('submit')) {
+      
+      // Mengatur validasi data password,
+      // # required = tidak boleh kosong
+      // # min_length[5] = password harus terdiri dari minimal 5 karakter
+      $this->form_validation->set_rules('password', 'Password', 'required|min_length[5]');
+
+      // Mengatur validasi data level,
+      // # required = tidak boleh kosong
+      // # in_list[administrator, alumni] = hanya boleh bernilai 
+      //   salah satu di antara administrator atau alumni
+      $this->form_validation->set_rules('level', 'Level', 'required|in_list[administrator,alumni]');
+
+      // Mengatur validasi data active,
+      // # required = tidak boleh kosong
+      // # in_list[0, 1] = hanya boleh bernilai 
+      // salah satu di antara 0 atau 1
+      $this->form_validation->set_rules('active', 'Active', 'required|in_list[0,1]');
+
+      // Mengatur pesan error validasi data
+      $this->form_validation->set_message('required', '%s tidak boleh kosong!');
+      $this->form_validation->set_message('min_length', '%s minimal %d karakter!');
+
+      // Jalankan validasi jika semuanya benar maka lanjutkan
+			if ($this->form_validation->run() === TRUE) {
+
+        $data = array(
+          'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
+          'level' => $this->input->post('level'),
+          'active' => $this->input->post('active')
+        );
+
+        // Jalankan function insert pada model_users
+        $query = $this->model_users->update($id, $data);
+
+        // cek jika query berhasil
+        if ($query) $message = array('status' => true, 'message' => 'Berhasil memperbarui user');
+        else $message = array('status' => true, 'message' => 'Gagal memperbarui user');
+
+        // simpan message sebagai session
+        $this->session->set_flashdata('message', $message);
+
+        // refresh page
+        redirect('users/edit/'.$id, 'refresh');
+			} 
+    }
+    
+    // Ambil data user dari database
+    $user = $this->model_users->get_where(array('id' => $id))->row();
+
+    // Jika data user tidak ada maka show 404
+    if (!$user) show_404();
+
+    // Data untuk page users/add
+    $data['pageTitle'] = 'Edit Data User';
+    $data['user'] = $user;
+    $data['pageContent'] = $this->load->view('users/userEdit', $data, TRUE);
+
+    // Jalankan view template/layout
+    $this->load->view('template/layout', $data);
+  }
+
+  public function delete($id)
+  {
+    // Ambil data user dari database
+    $user = $this->model_users->get_where(array('id' => $id))->row();
+
+    // Jika data user tidak ada maka show 404
+    if (!$user) show_404();
+
+    // Jalankan function delete pada model_users
+    $query = $this->model_users->delete($id);
+
+    // cek jika query berhasil
+    if ($query) $message = array('status' => true, 'message' => 'Berhasil menghapus user');
+    else $message = array('status' => true, 'message' => 'Gagal menghapus user');
+
+    // simpan message sebagai session
+    $this->session->set_flashdata('message', $message);
+
+    // refresh page
+    redirect('users', 'refresh');
   }
 }
--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -1,5 +1,6 @@
 <aside>
   <ul id="sidenav" class="side-nav fixed">
+    
     <li>
       <div class="userView">
         <div class="background">
@@ -10,11 +11,30 @@
         <a href="#!email"><span class="white-text email">nafplann@gmail.com</span></a>
       </div>
     </li>
+
     <li>
-      <a class="waves-effect"  href="<?php echo base_url('dashboard'); ?>"><i class="material-icons">home</i>Dashboard</a>
+      <a class="waves-effect" href="<?php echo base_url('dashboard'); ?>"><i class="material-icons">home</i>Dashboard</a>
     </li>
+    
+    <li>
+      <div class="divider"></div>
+    </li>
+
+    <li>
+      <a class="subheader">Admin</a>
+    </li>
+    
+    <li>
+      <a class="waves-effect" href="<?php echo base_url('users'); ?>"><i class="material-icons">people</i>Users</a>
+    </li>
+    
+    <li>
+      <div class="divider"></div>
+    </li>
+    
     <li>
       <a class="waves-effect"  href="<?php echo base_url('auth/logout'); ?>"><i class="material-icons">exit_to_app</i>Logout</a>
     </li>
+    
   </ul>
 </aside>
