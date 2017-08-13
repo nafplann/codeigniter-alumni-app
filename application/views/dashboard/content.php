5fbbfd6466428ab689010e287b4e79ab303f4b12
Abdul Manaaf <nafplann@gmail.com>
2017-08-13 16:22:09 +0800

    upload part15

--- application/views/dashboard/content.php
+++ application/views/dashboard/content.php
@@ -21,7 +21,7 @@
     </div>
     <div class="col s12 m6">
         <div class="card red darken-1">
-            <div class="card-content white-text x">
+            <div class="card-content white-text xx">
                 <p class="card-title">Lowongan Kerja Terbaru</p>
                 <div class="row dashboard-row">
                     <div class="col s2">
